33e8a08d0c461beae7eeb1630987aef69e04fcf8
EvanStrand2022 <818584@mystma.org>
2021-09-28 09:12:02 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE HERE
+#define function
+def addCheck(x, y, z):
+    #define variables
+    addCheck (2, 2, 4)
+    if x + y == z:
+        return True
+    else:
+        return False
